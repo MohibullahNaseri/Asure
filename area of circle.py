ec68b8d08eb8a062d5375570e8488c7ae00359d1
MohibullahNaseri <77476606+MohibullahNaseri@users.noreply.github.com>
2021-03-08 16:49:44 +0530

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,7 @@
+#python program to calculate area of a circle
+print("Name: Mohibullah\nR/No: 1/19/FET/BCS/104\n")
+
+radius = input("Enter the radius: ")
+r = int(radius)
+area = 3.14*(r**2)
+print("area of circle:", area)
