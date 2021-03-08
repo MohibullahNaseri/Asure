f97fbdab81451531d9757da4ff9a149afeb92349
MohibullahNaseri <77476606+MohibullahNaseri@users.noreply.github.com>
2021-03-08 17:10:53 +0530

    Update area of circle.py

--- area of circle.py
+++ area of circle.py
@@ -1,6 +1,6 @@
 #python program to calculate area of a circle
 print("Name: Mohibullah\nR/No: 1/19/FET/BCS/104\n")
-print("Branch change\n")
+print("Branch change")
 
 radius = input("Enter the radius: ")
 r = int(radius)
